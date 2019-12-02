2225c2bdf6da51492e7836146f63147f8eb26c62
Presnia <presnia@tut.by>
2019-12-02 11:20:53 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var money = 100;
+var income = 1000;
+var addExpenses = 500;
+var deposit = 2000;
+var mission = 700;
+var period = "year";
+
+alert('Привет, Академия!');
+
+console.log('hi, folks');
